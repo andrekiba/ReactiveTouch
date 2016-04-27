eb44d76d924fed14c0796566c577d577cd86c09d
Andrea <andre.kiba@gmail.com>
2016-04-27 07:49:48 +0200

    update

--- ReactiveTouch/Pages/DrawLinesPage.xaml.cs
+++ ReactiveTouch/Pages/DrawLinesPage.xaml.cs
@@ -6,44 +6,69 @@ using MR.Gestures;
 using NControl.Abstractions;
 using ReactiveTouch.Model;
 using Xamarin.Forms;
+using Button = Xamarin.Forms.Button;
 
 namespace ReactiveTouch.Pages
 {
 	public partial class DrawLinesPage : Xamarin.Forms.ContentPage
 	{
-        private double width = 0;
-        private double height = 0;
+        private double width;
+        private double height;
 
         private readonly MR.Gestures.RelativeLayout layer1;
 	    private readonly MR.Gestures.RelativeLayout layer2;
-	    private readonly MR.Gestures.AbsoluteLayout layout;
+	    //private readonly MR.Gestures.AbsoluteLayout layout;
 
-		public DrawLinesPage()
+	    public DrawLinesPage()
         {
-            InitializeComponent();
+            #region Setup
 
-			BackgroundColor = Xamarin.Forms.Color.Black;
+		    InitializeComponent();
 
-			layer1 = new MR.Gestures.RelativeLayout();
-			layer2 = new MR.Gestures.RelativeLayout();
-			layout = new MR.Gestures.AbsoluteLayout { BackgroundColor = Xamarin.Forms.Color.Gray };
+            layer1 = new MR.Gestures.RelativeLayout();
+            layer2 = new MR.Gestures.RelativeLayout();
+            //layout = new MR.Gestures.AbsoluteLayout { BackgroundColor = Color.Gray };
 
-			layout.Children.Add(layer1);
-			layout.Children.Add(layer2);
+            MainLayout.Children.Add(layer1);
+            MainLayout.Children.Add(layer2);
 
-			Xamarin.Forms.AbsoluteLayout.SetLayoutFlags(layer1, AbsoluteLayoutFlags.SizeProportional);
+            Xamarin.Forms.AbsoluteLayout.SetLayoutFlags(layer1, AbsoluteLayoutFlags.SizeProportional);
             Xamarin.Forms.AbsoluteLayout.SetLayoutFlags(layer1, AbsoluteLayoutFlags.PositionProportional);
-            Xamarin.Forms.AbsoluteLayout.SetLayoutBounds(layer1, new Xamarin.Forms.Rectangle(0f, 0f, 1f, 1f));
+            //Xamarin.Forms.AbsoluteLayout.SetLayoutFlags(layer1, AbsoluteLayoutFlags.All);
+            Xamarin.Forms.AbsoluteLayout.SetLayoutBounds(layer1, new Rectangle(0, 0, 1, 1));
 
             Xamarin.Forms.AbsoluteLayout.SetLayoutFlags(layer2, AbsoluteLayoutFlags.SizeProportional);
             Xamarin.Forms.AbsoluteLayout.SetLayoutFlags(layer2, AbsoluteLayoutFlags.PositionProportional);
-            Xamarin.Forms.AbsoluteLayout.SetLayoutBounds(layer2, new Xamarin.Forms.Rectangle(0f, 0f, 1f, 1f));
+            //Xamarin.Forms.AbsoluteLayout.SetLayoutFlags(layer2, AbsoluteLayoutFlags.All);
+            Xamarin.Forms.AbsoluteLayout.SetLayoutBounds(layer2, new Rectangle(0, 0, 1, 1));
 
-			Content = layout;
+            #region Clear
 
-			#region RectiveX
+            var clearButton = new Button
+            {
+                Text = "Clear",
+                TextColor = Color.White
+            };
+            clearButton.Clicked += (sender, args) =>
+            {
+                layer1.Children.Clear();
+                layer2.Children.Clear();
+            };
 
-			Func<EventPattern<DownUpEventArgs>, NGraphics.Point> GetPosition = x =>
+            Xamarin.Forms.AbsoluteLayout.SetLayoutFlags(clearButton, AbsoluteLayoutFlags.All);
+            Xamarin.Forms.AbsoluteLayout.SetLayoutBounds(clearButton, new Rectangle(0, 0, 1, 0.10));
+
+            //MainLayout.Children.Add(clearButton);
+
+            #endregion
+
+            Content = MainLayout;
+
+            #endregion
+
+            #region RectiveX
+
+            Func<EventPattern<DownUpEventArgs>, NGraphics.Point> getPosition = x =>
 			{
 				Debug.WriteLine("X " + x.EventArgs.Center.X + " Y " + x.EventArgs.Center.Y);
 				return new NGraphics.Point(x.EventArgs.Center.X, x.EventArgs.Center.Y);
@@ -51,10 +76,10 @@ namespace ReactiveTouch.Pages
 
 			var points = Observable
 				.FromEventPattern<DownUpEventArgs>(
-					handler => layout.Up += handler,
-					handler => layout.Up -= handler
+					handler => MainLayout.Up += handler,
+					handler => MainLayout.Up -= handler
 					)
-				.Select(GetPosition);
+				.Select(getPosition);
 
 			points
 				.Subscribe(DrawCircle);
@@ -90,10 +115,41 @@ namespace ReactiveTouch.Pages
 
         }
 
+        private void DrawCircle(NGraphics.Point p)
+        {
+            const double dim = 20;
+
+            layer2.Children.Add(new CircleControl
+                {
+                    FillColor = Xamarin.Forms.Color.FromRgb(252, 37, 145),
+                    HeightRequest = dim,
+                    WidthRequest = dim
+                },
+                Constraint.RelativeToParent((parent) => p.X - dim * .5),
+                Constraint.RelativeToParent((parent) => p.Y - dim * .5)
+            );
+        }
+
+        private void DrawLine(Line line)
+        {
+            layer1.Children.Add(new LineControl(line.Start, line.End)
+                {
+                    Thickness = 4,
+                    FillColor = Xamarin.Forms.Color.FromRgb(89, 89, 89),
+                    HeightRequest = Height,
+                    WidthRequest = Width
+                },
+                Constraint.RelativeToParent((parent) => 0),
+                Constraint.RelativeToParent((parent) => 0)
+            );
+        }
+
+        #region Test
+
         protected override void OnSizeAllocated(double w, double h)
         {
             base.OnSizeAllocated(w, h);
-            
+
             if (width != w || height != h)
             {
                 width = w;
@@ -111,31 +167,6 @@ namespace ReactiveTouch.Pages
             }
         }
 
-        void DrawCircle(NGraphics.Point p)
-		{
-		    const double dim = 20;
-
-		    layer2.Children.Add(new CircleControl { FillColor = Xamarin.Forms.Color.FromRgb(252, 37, 145),
-													HeightRequest = dim,
-													WidthRequest = dim },
-			                 Constraint.RelativeToParent((parent) => p.X - dim * .5),
-							 Constraint.RelativeToParent((parent) => p.Y - dim * .5)
-							);
-		}
-
-	    void DrawLine(Line line)
-		{
-			layer1.Children.Add(new LineControl(line.Start, line.End)
-			{
-													Thickness = 4,
-													FillColor = Xamarin.Forms.Color.FromRgb(89, 89, 89),
-													HeightRequest = Height,
-													WidthRequest = Width
-												}, 
-			                 Constraint.RelativeToParent((parent) => 0),
-			                 Constraint.RelativeToParent((parent) => 0)	
-			                );			                		
-		}
-
-    }
+        #endregion
+	}
 }
